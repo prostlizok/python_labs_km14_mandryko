142c03a0c4aee957cf303203a58fbdb48f65fb94
Liza Mandryko <prostlizok@gmail.com>
2021-11-20 08:21:21 +0200

    eleventh prac

--- p11_mandryko/p11_mandryko_1.py
+++ p11_mandryko/p11_mandryko_1.py
@@ -1,3 +1,12 @@
+def cons(head, tail=[]):
+    res = []
+    if tail == []:
+        res.append(int(head))
+        return res
+    else:
+        res.append(head)
+        res.extend(tail)
+        return res
 
 def sum(l):
    if len(l) == 1:
@@ -5,6 +14,12 @@ def sum(l):
    else:
         return l[0] + sum(l[1:])
 
+l = cons(3, cons(2, cons(1, [])))
+print(f'Result: {l}')
+
+assert l == [3, 2, 1], 'Failed test 1'
+assert cons(1) == [1], 'Failed test 2'
+print('All tests good!')
 #print(sum([1, 2, 3]))
-assert sum([1, 2 ,3]) == 6, 'Failed on sum'
+assert sum(l) == 6, 'Failed on sum'
 print('All tests good!')
